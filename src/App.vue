17744e14c1331c3a66c11deff33138246f29e34e
Chanze Wu <chanze7995@gmail.com>
2022-05-07 01:35:58 +0800

    Retrieve Firebase Data

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="app">
-    <Navigation />
-    <div class="appContainer">
-      <AlertModal v-if="isAlertModalActive"/>
-      <transition name="invoiceModal">
-        <InvoiceModal v-if="isInvoiceModalOpen"/>
-      </transition>
-      <router-view />
+  <div v-if="isInvoicesLoaded">
+    <div class="app">
+      <Navigation />
+      <div class="appContainer">
+        <AlertModal v-if="isAlertModalActive"/>
+        <transition name="invoiceModal">
+          <InvoiceModal v-if="isInvoiceModalOpen"/>
+        </transition>
+        <router-view />
+      </div>
     </div>
   </div>
 </template>
@@ -16,7 +18,7 @@ import Navigation from '@/components/Navigation.vue'
 import InvoiceModal from '@/components/InvoiceModal.vue'
 import AlertModal from '@/components/AlertModal.vue'
 
-import { computed } from 'vue'
+import { computed, onMounted } from 'vue'
 import { useStore } from 'vuex'
 
 export default {
@@ -28,13 +30,23 @@ export default {
   setup () {
     const store = useStore()
 
+    const getInvoiceData = () => {
+      return store.dispatch('getInvoiceData')
+    }
+    const isInvoicesLoaded = computed(() => {
+      return store.getters.isInvoicesLoaded
+    })
     const isInvoiceModalOpen = computed(() => {
       return store.getters.isInvoiceModalOpen
     })
     const isAlertModalActive = computed(() => {
       return store.getters.isAlertModalActive
     })
+    onMounted(() => {
+      getInvoiceData()
+    })
     return {
+      isInvoicesLoaded,
       isInvoiceModalOpen,
       isAlertModalActive
     }
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,49 @@
 import { createStore } from 'vuex'
+import db from '../firebase/firebaseInit'
 
 export default createStore({
   state: {
+    invoiceData: [],
     isInvoiceModalOpen: false,
-    isAlertModalActive: false
+    isAlertModalActive: false,
+    isInvoicesLoaded: null
   },
   actions: {
+    async getInvoiceData ({ commit, state }) {
+      const getData = db.collection('invoices')
+      const results = await getData.get()
+      results.forEach((doc) => {
+        if (!state.invoiceData.some((invoice) => invoice.docId === doc.id)) {
+          const data = {
+            docId: doc.id,
+            invoiceId: doc.data().invoiceId,
+            billerStreetAddress: doc.data().billerStreetAddress,
+            billerCity: doc.data().billerCity,
+            billerZipCode: doc.data().billerZipCode,
+            billerCountry: doc.data().billerCountry,
+            clientName: doc.data().clientName,
+            clientEmail: doc.data().clientEmail,
+            clientStreetAddress: doc.data().clientStreetAddress,
+            clientCity: doc.data().clientCity,
+            clientZipCode: doc.data().clientZipCode,
+            clientCountry: doc.data().clientCountry,
+            invoiceDateUnix: doc.data().invoiceDateUnix,
+            invoiceDate: doc.data().invoiceDate,
+            paymentTerms: doc.data().paymentTerms,
+            paymentDueDateUnix: doc.data().paymentDueDateUnix,
+            paymentDueDate: doc.data().paymentDueDate,
+            productDescription: doc.data().productDescription,
+            invoiceItemList: doc.data().invoiceItemList,
+            invoiceTotal: doc.data().invoiceTotal,
+            invoicePending: doc.data().invoicePending,
+            invoiceDraft: doc.data().invoiceDraft,
+            invoicePaid: doc.data().invoicePaid
+          }
+          commit('SET_INVOICE_DATA', data)
+        }
+      })
+      commit('SET_INVOICES_LOADED')
+    },
     toggleInvoiceModalOpen (context) {
       context.commit('TOGGLE_INVOICE')
     },
@@ -14,6 +52,13 @@ export default createStore({
     }
   },
   mutations: {
+    SET_INVOICE_DATA (state, payload) {
+      state.invoiceData.push(payload)
+      console.log(state.invoiceData)
+    },
+    SET_INVOICES_LOADED (state) {
+      state.isInvoicesLoaded = true
+    },
     TOGGLE_INVOICE (state) {
       state.isInvoiceModalOpen = !state.isInvoiceModalOpen
     },
@@ -22,6 +67,12 @@ export default createStore({
     }
   },
   getters: {
+    invoiceData (state) {
+      return state.invoiceData
+    },
+    isInvoicesLoaded (state) {
+      return state.isInvoicesLoaded
+    },
     isInvoiceModalOpen (state) {
       return state.isInvoiceModalOpen
     },
