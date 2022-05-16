15384359b042e227cff1560717a192f74fd8f953
Chanze Wu <chanze7995@gmail.com>
2022-05-16 14:48:44 +0800

    Set Filter by Status

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,7 @@
       </div>
       <div class="headerFnContainer">
         <div @click="toggleFilterMenu" class="filter">
-          <span>Filter by status</span>
+          <span>Filter by status<span v-if="filteredInvoice">: {{ filteredInvoice }}</span></span>
           <img src="@/assets/icons/icon-arrow-down.svg" alt="" />
           <ul v-show="isFilterMenuOpen" class="filter-menu">
             <li @click="filteredInvoices">Draft</li>
@@ -25,8 +25,7 @@
       </div>
     </div>
     <div v-if="invoiceData.length > 0">
-      <!-- <Invoice v-for="(invoice, index) in filteredData" :invoice="invoice" :key="index" /> -->
-      <Invoice v-for="(invoice, index) in invoiceData" :invoice="invoice" :key="index" />
+      <Invoice v-for="(invoice, index) in filteredData" :invoice="invoice" :key="index" />
     </div>
     <div v-else class="empty">
       <img src="@/assets/images/illustration-empty.svg" alt="" />
@@ -49,6 +48,7 @@ export default {
   setup () {
     const store = useStore()
     const isFilterMenuOpen = ref(false)
+    const filteredInvoice = ref(null)
     const toggleFilterMenu = () => {
       isFilterMenuOpen.value = !isFilterMenuOpen.value
     }
@@ -56,13 +56,39 @@ export default {
       store.dispatch('toggleInvoiceModalOpen')
     }
     const invoiceData = computed(() => {
+      console.log(store.getters.invoiceData)
       return store.getters.invoiceData
     })
+    const filteredInvoices = (e) => {
+      if (e.target.innerText === 'Clear Filter') {
+        filteredInvoice.value = null
+        return
+      }
+      filteredInvoice.value = e.target.innerText
+    }
+    const filteredData = computed(() => {
+      return invoiceData.value.filter((invoice) => {
+        if (filteredInvoice.value === 'Draft') {
+          return invoice.invoiceDraft === true
+        }
+        if (filteredInvoice.value === 'Pending') {
+          return invoice.invoicePending === true
+        }
+        if (filteredInvoice.value === 'Paid') {
+          return invoice.invoicePaid === true
+        }
+        return invoice
+      })
+    })
+
     return {
       isFilterMenuOpen,
+      filteredInvoice,
       toggleFilterMenu,
       newInvoiceModal,
-      invoiceData
+      invoiceData,
+      filteredInvoices,
+      filteredData
     }
   }
 }
